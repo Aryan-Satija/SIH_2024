0afcc3d1a0f8910549e142f72859e0c337657d6a
aryan <aryansatija2003@gmail.com>
2024-09-26 19:03:08 +0530

    hardat config files

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.27",
+  networks:{
+    sepolia:{
+      url: "https://eth-sepolia.g.alchemy.com/v2/GRIRfkS8p_KDIhtPgEcauWBfjucLFolH",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
